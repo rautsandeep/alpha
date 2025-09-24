a095aa5151e2cc4bd385474f090ea1db66733bf4
Snehal <snehalshinde4763.com>
2025-09-24 23:27:27 +0530

    add

---
+++ father/father.go
@@ -0,0 +1,5 @@
+package father
+
+func Test() string{
+	return "This string is returned from the test func from the father package")
+}
---
+++ first.go
@@ -0,0 +1,15 @@
+package main
+
+
+import ("fmt"
+"github.com/rautsandeep/alpha/father")
+
+func main() {
+
+fmt.Println("This is from the alpha package")
+alpha.Test()
+
+
+}
+
+
