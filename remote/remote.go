767a1e5afeea98fc78685a4f30344b8211dd97be
rautsandeep <55024789+rautsandeep@users.noreply.github.com>
2025-12-06 16:45:14 +0530

    Update remote.go

--- remote/remote.go
+++ remote/remote.go
@@ -4,7 +4,7 @@ import(
   "fmt"
 )
 
-func Apac() string {
-  return fmt.Println("this input is from the apac remote region")
+func Apac(p sring) string {
+  return fmt.Println("this input is from the apac remote region",p)
 
   }
